cfd16b8479b6b7b04f3a03f15a46ac27d49e2dc5
BrainsCode <60605857+yanuarnval@users.noreply.github.com>
2020-11-14 12:13:05 +0700

    update 2

---
+++ training.js
@@ -0,0 +1,13 @@
+var person = {
+    name,
+    age,
+    addres,
+    job
+};
+
+function person(name, age, addres, job) {
+    this.name = name;
+    this.age = age;
+    this.job = job;
+    this.addres = addres;
+}
